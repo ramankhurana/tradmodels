70d5ee00fe2500f7d8ada2fcd4e8179739c004e6
Raman Khurana <ramankhurana1986@gmail.com>
2024-05-13 13:58:48 -0500

    updated submit-job.sh for multiple datasets

--- submit-job.sh
+++ submit-job.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 templateyaml=cluster-run/job-tiger.yaml
 # Define datasets and models
-declare -a datasets=("ETTh1")
+declare -a datasets=("ETTh1" "ETTh2")
 declare -a models=("ARIMA")
 
 # Loop through datasets and models
 for dataset in "${datasets[@]}"; do
   for model in "${models[@]}"; do
-      # Replace the dataset and model names in a job template and apply it
-      sed "s/TEMPLATE_DATASET_NAME/$dataset/g; s/TEMPLATE_MODEL_NAME/$model/g" $templateyaml | kubectl apply -f -
+
+    # Create a sanitized version of dataset and model names for use in Kubernetes object names, it should be in lower case
+    clean_dataset=$(echo $dataset | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+    clean_model=$(echo $model | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+
+    job_name="tradmodels-${clean_model}-${clean_dataset}-$(date +%Y%m%d%H%M%S)"
+    echo "submitting job for:" $job_name
+
+    # Replace the dataset and model names in a job template and apply it
+    sed -e "s/TEMPLATE_DATASET_NAME/$dataset/g" \
+        -e "s/TEMPLATE_MODEL_NAME/$model/g" \
+        -e "s/tradmodels-job/$job_name/g" $templateyaml | kubectl apply -f -
+
   done
 done
