2714ccad2b1704aa7c7861a1dfa4866d7b0c3f3e
Raman Khurana <ramankhurana1986@gmail.com>
2024-06-10 10:59:44 -0500

    runtest shell script to run all the experiments for TimeGPT

---
+++ runtest.sh
@@ -0,0 +1,11 @@
+sed -i '' 's/horizon: 6/horizon: 12/' datasetschema.yaml
+python EvaluateModel.py --dataset=ETTh1 --model=TimeGPT
+python EvaluateModel.py --dataset=ETTh2 --model=TimeGPT
+python EvaluateModel.py --dataset=ETTm1 --model=TimeGPT
+python EvaluateModel.py --dataset=ETTm2 --model=TimeGPT
+python EvaluateModel.py --dataset=Electricity --model=TimeGPT
+python EvaluateModel.py --dataset=Exchange --model=TimeGPT
+python EvaluateModel.py --dataset=Weather --model=TimeGPT
+python EvaluateModel.py --dataset=Illness --model=TimeGPT
+python EvaluateModel.py --dataset=Divvy --model=TimeGPT
+python EvaluateModel.py --dataset=M5 --model=TimeGPT
