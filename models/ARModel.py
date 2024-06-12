0b57ef5134454313a183d10bdde095b865037116
Raman Khurana <ramankhurana1986@gmail.com>
2024-06-12 13:07:31 -0500

    change AR model for correct step forward evaluation of model

--- models/ARModel.py
+++ models/ARModel.py
@@ -4,7 +4,6 @@ AR = ARIMA(lag,d=0)
 ARIMA = ARIMA(lag,d=1)
 in both cases MA is turned off for the moment as it was not improving the MSE score for a few datasets I tried, ETT and Divvy 
 '''
-
 from BaseModel import BaseModel
 from darts.models import ARIMA
 from darts import TimeSeries
@@ -15,67 +14,7 @@ from ForecastMetrics import ForecastMetrics
 class ARModel(BaseModel):
 
     
-    
-    def fit_predict(self):
-        results = {}
-        mse_scores = {}
-        all_actual = []
-        all_predicted = []
-        self.lag = self.dataset_info['lag']
-        self.horizon = self.dataset_info['horizon']
-
-        ## from yaml file
-        #train_start,train_end=self.dataset_info["train"]
-        #val_start,val_end=self.dataset_info["val"]
-        
-        train_start,train_end=self.train_
-        val_start,val_end=self.val_
-        test_start,test_end=self.test_
-                
-        
-        for column in self.usable_cols:
-            series = TimeSeries.from_dataframe(self.data, self.dataset_info['date_col'], column,
-                                               fill_missing_dates=True)# , freq='10T')
-            #train, test = series.split_after(pd.Timestamp(self.dataset_info['test'][0]))
-
-            # Splitting based on indices rather than dates
-            train = series[:train_end + 1]  # includes the train_end index
-            test = series[val_end + 1:]  # starts just after train_end
-
-            print ("----------------------",train.shape, test.shape)
-            
-            model = ARIMA(p=self.dataset_info['lag'],
-                          d=0)
-            model.fit(train)
-            prediction = model.predict(len(test))
-
-            # Collect individual predictions for MSE calculation
-            actual_values = test.values().flatten()
-            predicted_values = prediction.values().flatten()
-
-            # Calculate MSE for the current column
-            mse = mean_squared_error(actual_values, predicted_values)
-            results[column] = prediction
-            mse_scores[column] = mse
-
-            # Append results for consolidated MSE calculation
-            all_actual.extend(actual_values)
-            all_predicted.extend(predicted_values)
-
-        # Calculate the aggregate MSE across all columns
-        aggregate_mse = np.mean(list(mse_scores.values()))
-
-        # Calculate consolidated MSE across all columns
-        consolidated_mse = mean_squared_error(all_actual, all_predicted)
-
-        return results, mse_scores, aggregate_mse, consolidated_mse
-
-
     def rolling_window_evaluation(self):
-        #self.lag = self.dataset_info['lag']
-        #self.horizon = self.dataset_info['horizon']
-
-        
         train_start,train_end=self.train_
         val_start,val_end=self.val_
         test_start,test_end=self.test_
@@ -88,31 +27,48 @@ class ARModel(BaseModel):
         # Split data into train and test based on predefined dates (can be set in dataset_info)
         series = TimeSeries.from_dataframe(self.data, self.dataset_info['date_col'], fill_missing_dates=True) #, freq='10T')
         
-        train = series[:val_end + 1]  ## train should include the validation as well otherwise there is unseen data which is equal to val data length. 
-        test = series[test_start + 1:] 
-        
         # Loop over each usable column
         for column in self.usable_cols:
             print ("column name", column)
-            train_series = train[column]
-            test_series = test[column]
-            model = ARIMA(p=self.lag,
-                          d=0)
-            
-            model.fit(train_series)
 
-            num_windows = len(test_series) - self.horizon + 1
+            
+            num_windows = (test_end-test_start) - self.horizon + 1
+            step_size = self.getStepSize(num_windows)
+            print ("num_windows,step_size: ", num_windows,step_size)
+            
             column_actuals = []
             column_forecasts = []
             
             # Perform rolling window predictions
             for start in range(num_windows):
+
+                if ( (self.dataset_info['name'] == "Illness") and (start % 10 !=0)  ) or  (start % step_size != 0)  :
+                    continue
+
+                
+                train = series[:val_end + 1 + start]   
+                test = series[test_start + 1 + start:]
+
+                print ("train.shape, test.shape", train.n_samples, train.n_timesteps, train.n_components )
+                
+                train_series = train[column]
+                test_series = test[column]
+
+                model = ARIMA(p=self.lag,
+                              d=0)
+                model.fit(train_series)
+                
+                prediction = model.predict(self.horizon)
+                predicted_values = prediction.values().flatten()
+
+                
                 end = start + self.horizon
                 test_slice = test_series[start:end]
-
-                prediction = model.predict(self.horizon)
                 actual_values = test_slice.values().flatten()
-                predicted_values = prediction.values().flatten()
+
+                print ("!!!!!!!!!!!!@@@@@@@@@@@@@@################  ------------------- prediction", predicted_values)
+                print ("!!!!!!!!!!!!@@@@@@@@@@@@@@################  ------------------- actuals", actual_values)
+                
 
                 column_actuals.extend(actual_values)
                 column_forecasts.extend(predicted_values)
